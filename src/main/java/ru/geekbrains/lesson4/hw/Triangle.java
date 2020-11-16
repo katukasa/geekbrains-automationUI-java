635eab753b8cb0daaff1f8251a94a405f2674d7f
zwr6sn <ekaterina.sheldukova@siemens.com>
2020-11-16 15:18:33 +0100

    lesson 4 hw square 2

--- src/main/java/ru/geekbrains/lesson4/hw/Triangle.java
+++ src/main/java/ru/geekbrains/lesson4/hw/Triangle.java
@@ -3,10 +3,10 @@ package ru.geekbrains.lesson4.hw;
 public class Triangle {
 
     public static void main(String[] args) {
-
+        triangleSquare(1,2,3);
     }
 
-    public void triangleSquare(double a, double b, double c){
+    public static void triangleSquare(double a, double b, double c){
         double p = (a + b + c) / 2;
         double square = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         System.out.println(square);
