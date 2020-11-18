8fda593c31a01461107087664f6f1efb8fe489b7
zwr6sn <ekaterina.sheldukova@siemens.com>
2020-11-18 21:37:39 +0100

    lesson 5 hw

--- src/main/java/ru/geekbrains/lesson5_hw/TestCase1.java
+++ src/main/java/ru/geekbrains/lesson5_hw/TestCase1.java
@@ -1,5 +1,6 @@
-package ru.geekbrains.lesson3.hw;
+package ru.geekbrains.lesson5_hw;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -18,10 +19,12 @@ public class TestCase1 {
     private static final String PASSWORD = "Student2020!";
     private static final String projectsMenu = "//ul[@class='nav nav-multilevel main-menu']/li[@class='dropdown']/a/span[contains(., 'Проекты')]";
     private static final String projectsSubMenu = "//span[@class='title' and text()='Мои проекты']";
-    private static final String button = "div[class='pull-left btn-group icons-holder']";
+    private static final String createProjectButton = "div[class='pull-left btn-group icons-holder']";
     private static final String contact = "//*[@id='select2-drop']/div/input";
     private static final String company = "//*[@id='select2-drop']/div/input";
     private static final String saveButton = "//button[@class='btn btn-success action-button']";
+    private static final String panelIntsHeader = "//h1[@class='oro-subtitle' and contains(text(), 'Панель инструментов')]";
+    private static final String createProjectHeader = "//h1[contains(text(), 'Создать проект')]";
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
@@ -32,49 +35,42 @@ public class TestCase1 {
         login();
 
 //      Step 2: Перейти в “Проекты” → “Мои проекты”
-        driver.findElement(By.xpath(projectsMenu)).click();
-        driver.findElement(By.xpath(projectsSubMenu)).click();
+        openMyProjects();
 
 //      Step 3: Нажать на кнопку “Создать проект”
-        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(button))));
-        driver.findElement(By.cssSelector(button)).click();
-
-        new WebDriverWait(driver, 5).until(ExpectedConditions.urlContains("/create"));
+        createNewProject();
 
 //      Step 4: Заполнить обязательные поля:
 //      Наименование (text) - works only in debug mode :(
         driver.findElement(By.name("crm_project[name]")).sendKeys("kat_project2");
-
 //      Организация (drop down) - через селекторы не работало, какой-то странный список, не такой как остальные
         driver.findElement(By.xpath("//span[@class='select2-chosen' and text()='Укажите организацию']")).click();
         new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(company))));
         driver.findElement(By.xpath(company)).sendKeys("Test Organisation_10");
         driver.findElement(By.xpath(company)).sendKeys(Keys.ENTER);
-
 //      Основное контактное лицо (drop down) - available only when "Организация" field is filled, тоже не работает через селекторы
         driver.findElement(By.xpath("//div[@class='select2-container select2']")).click();
         new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(contact))));
         driver.findElement(By.xpath(contact)).sendKeys("Мартынов Николай");
         driver.findElement(By.xpath(contact)).sendKeys(Keys.ENTER);
-
 //      Подразделение (drop down) - done
         Select businessUnitDropDown = new Select(driver.findElement(By.name("crm_project[businessUnit]")));
         businessUnitDropDown.selectByValue("1");
-
 //      Куратор проекта (drop down) - done
         Select curatorDropDown = new Select(driver.findElement(By.xpath("//select[@name='crm_project[curator]']")));
         curatorDropDown.selectByValue("5");
-
 //      Руководитель проекта (drop down)
         Select headDropDown = new Select(driver.findElement(By.xpath("//select[@name='crm_project[rp]']")));
         headDropDown.selectByValue("5");
-
 //      Менеджер (drop down)
         Select managerDropDown = new Select(driver.findElement(By.xpath("//select[@name='crm_project[manager]']")));
         managerDropDown.selectByValue("5");
+//      Проверить, что все поля заполнены правильно
 
 //      Step 5: Нажать на кнопку “Сохранить и закрыть”
         driver.findElement(By.xpath(saveButton)).click();
+//      пользователь видит страницу “Все проекты”
+//      и всплывающее уведомление о том, что проект успешно создан
 
         tearDown();
     }
@@ -90,10 +86,31 @@ public class TestCase1 {
 
         WebElement loginButton = driver.findElement(By.xpath(".//button[@name='_submit']"));
         loginButton.click();
+
+//      Пользователь видит страницу Панель Инструментов
+        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(panelIntsHeader))));
+        Assertions.assertTrue(driver.findElement(By.xpath(panelIntsHeader)).isDisplayed());
+    }
+
+    private static void openMyProjects() {
+        driver.findElement(By.xpath(projectsMenu)).click();
+        driver.findElement(By.xpath(projectsSubMenu)).click();
+
+//      Присутствует кнопка “Создать проект”
+        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(createProjectButton))));
+        Assertions.assertTrue(driver.findElement(By.xpath(createProjectButton)).isDisplayed());
+    }
+
+    private static void createNewProject() {
+        driver.findElement(By.cssSelector(createProjectButton)).click();
+        new WebDriverWait(driver, 5).until(ExpectedConditions.urlContains("/create"));
+
+//      Присутствует заголовок “Создать проект”
+        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(createProjectHeader))));
+        Assertions.assertTrue(driver.findElement(By.xpath(createProjectHeader)).isDisplayed());
     }
 
     private static void tearDown(){
-        System.out.println("Test Case 1 is completed.");
         driver.quit();
     }
 
